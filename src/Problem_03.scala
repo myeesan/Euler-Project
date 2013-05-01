4c1129cd2ea97d3b2b2a23ab2c67a12b474d108d
Brandon JeongYeol Choi <myeesan@gmail.com>
2013-05-01 18:54:43 +0900

    add require() in case input under 1

--- src/Problem_03.scala
+++ src/Problem_03.scala
@@ -1,17 +1,21 @@
 import scala.annotation.tailrec
 
 // http://euler.synap.co.kr/prob_detail.php?id=3
-object Problem_03 extends App{
+object Problem_03 extends App {
 
-  def largestPrimeFactor(x: Long): Long = {
+  def largestPrimeFactor(x: Long) = {
+    require(x > 1L)
     @tailrec
-    def nextPrime(x: Long, y: Long): Long = {
-      if (x == y) y
-      else if (x % y == 0) nextPrime(x / y, y)
-      else nextPrime(x, y + 1)
+    def nextPrime(t: Long, d: Long): Long = {
+      if (t == d) d
+      else if (t % d == 0) nextPrime(t / d, d)
+      else nextPrime(t, d + 1)
     }
     nextPrime(x, 2)
   }
-  
+
+  //  println(largestPrimeFactor(1))
   println(largestPrimeFactor(600851475143L))
+  println(largestPrimeFactor(12352312353L))
+  println(largestPrimeFactor(3235231235323653616L))
 }
